0f1d715179ba12a499387130093f325ea0bee735
jbreaux <jbreaux@governmentcio.com>
2020-12-07 07:53:50 -0600

    added another project

---
+++ ReadInSampleFile126/DAL/SqlDataAccess.cs
@@ -0,0 +1,199 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+
+namespace ReadInSampleFile126.DAL
+{
+    public class SQLDataAccess
+    {
+
+        protected string ConnectionString { get; set; }
+        protected SqlConnection _conn;
+
+        public SQLDataAccess()
+        {
+            //var configuration = new ConfigurationBuilder()
+            //    .SetBasePath(Directory.GetParent(Directory.GetCurrentDirectory()) + "/MyProject.DAL").AddJsonFile("config.json", false)
+            //    .Build();
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.dev.json")
+                .Build();
+
+            this.ConnectionString = config.GetSection("Connections:connectionString").Value;
+            _conn = GetConnection();
+        }
+
+        private SqlConnection GetConnection()
+        {
+            SqlConnection connection = new SqlConnection(this.ConnectionString);
+            if (connection.State != ConnectionState.Open)
+                connection.Open();
+            return connection;
+        }
+        public int InsertHeader(string hdrFileDate)
+        {
+            int rows = 0;
+         
+            try
+            {
+                if (_conn.State != ConnectionState.Open)
+                    _conn.Open();
+
+                string query =
+                    $"INSERT INTO tblFas126 (hdrFileDate, dateCreated, comment, fileNumber) VALUES ('{hdrFileDate}', '{DateTime.UtcNow}', '{"Header Data"}', '0')";
+                rows = InsertDb(query);
+            }
+            catch (Exception ex)
+            {
+                rows = -1;
+            }
+            return rows;
+        }
+        public int InsertTrailer(string fileDate, string nRecords)
+        {
+            int rows = 0;
+            try
+            {
+                if (_conn.State != ConnectionState.Open)
+                    _conn.Open();
+
+                string query =
+                    $"INSERT INTO tblFas126 (trailerFileDate, trailerNumRecords, dateCreated, comment, fileNumber) VALUES ('{fileDate}', '{nRecords}', '{DateTime.UtcNow}', '{"Trailer Record"}', '0')";
+                rows = InsertDb(query);
+            }
+            catch (Exception ex)
+            {
+                rows = -1;
+            }
+            return rows;
+        }
+
+        //sql.InsertBody(accountNumber, nameCode, opCode, loanCode,
+        //amount, fileDate, fileNumber);
+        public int InsertBody(string accountNumber, string nameCode, string opCode,
+            string loanCode, string amount, string fileDate, string fileNumber)
+        {
+            int rows = 0;
+            try
+            {
+                if (_conn.State != ConnectionState.Open)
+                    _conn.Open();
+
+                string query =
+                    $"INSERT INTO tblFas126 (accountNumber, nameCode, opCode, loanCode, amount, fileDate, fileNumber, dateCreated, comment) " + $"VALUES ('{accountNumber}', '{nameCode}','{opCode}','{loanCode}','{amount}','{fileDate}','{fileNumber}','{DateTime.UtcNow}', '{"Body Record"}')";
+                rows = InsertDb(query);
+            }
+            catch (Exception ex)
+            {
+                rows = -1;
+            }
+            return rows;
+        }
+        public int InsertAuditDb(string rawData)
+        {
+            int rows = 0;
+         
+            try
+            {
+                if (_conn.State != ConnectionState.Open)
+                    _conn.Open();
+
+                string query =
+                    $"INSERT INTO tblAudit (rawData, timeStamp, comment) VALUES ('{rawData}', '{DateTime.UtcNow}', '{"No comment"}')";
+                rows = InsertDb(query);
+            }
+            catch (Exception ex)
+            {
+                rows = -1;
+            }
+            return rows;
+        }
+
+        // generic, pass in query like insert into tablename(....) values(....)
+        private int InsertDb(string query)
+        {
+            int nRows = 0;
+            try
+            {
+                if (_conn.State != ConnectionState.Open)
+                    _conn.Open();
+
+                SqlCommand command = new SqlCommand(query, _conn);
+
+                if (_conn.State == ConnectionState.Closed)
+                    _conn.Open();
+
+                nRows = command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return nRows;
+        }
+
+        // generic, select xyz from tablename where abc
+        public void QueryDb(string query)
+        {
+            using (_conn)
+            {
+                if (_conn.State != ConnectionState.Open)
+                    _conn.Open();
+                SqlCommand command = new SqlCommand(
+                    query,
+                    //"SELECT CategoryID, CategoryName FROM Categories;",
+                    _conn);
+
+                if (_conn.State == ConnectionState.Closed)
+                    _conn.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0}\t{1}", reader.GetString(0),
+                            reader.GetString(1));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+                reader.Close();
+            }
+        }
+
+
+        //public DbDataReader GetDataReader(string procedureName, List<SqlParameter> parameters, CommandType commandType = CommandType.StoredProcedure)
+        //{
+        //    DbDataReader dr = null;
+        //    try
+        //    {
+        //        DbConnection connection = this.GetConnection();
+        //        {
+        //            //DbCommand cmd = this.GetCommand(connection, procedureName, commandType);
+        //            //if (parameters != null && parameters.Count > 0)
+        //            //{
+        //            //    cmd.Parameters.AddRange(parameters.ToArray());
+        //            //}
+        //            //dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw;
+        //    }
+        //    return dr;
+        //}
+    }
+}
+
---
+++ ReadInSampleFile126/Program.cs
@@ -0,0 +1,173 @@
+﻿using Exception = System.Exception;
+using Random = System.Random;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using ReadInSampleFile126.Utilities;
+using ReadInSampleFile126.DAL;
+
+namespace ReadInSampleFile126
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.dev.json")
+                .Build();
+
+            var targetFile = config.GetSection("InputFile1").Get<AppConfiguration.TargetFile>();
+            
+            ReadInputFile126(targetFile.path, targetFile.filename);
+        }
+
+        private static void ReadInputFile126( string path, string filename)
+        {
+            var sql = new SQLDataAccess();
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.  
+            StreamReader file =
+                new StreamReader(path + filename);
+            while ((line = file.ReadLine()) != null)
+            {
+                sql.InsertAuditDb(line);
+
+                counter++;
+                if (counter == 1)
+                    ProcessHeader(sql, line);
+                else
+                {
+                    if (string.Compare("003E", line.Substring(0, 4)) == 0)
+                    {
+                        break;
+                    }
+                    ProcessBodyRecord(sql, line);
+                }
+            }
+
+            ProcessTrailer(sql, line);
+
+            file.Close();
+        }
+
+        private static void ProcessBodyRecord(SQLDataAccess sql, string line)
+        {
+            //006E9155795930017 03    JJB   1L   274858       2003  401300575\
+            var staticData1 = line.Substring(0, 4);
+            var accountNumber = line.Substring(4, 9);
+            var nameCode = line.Substring(24, 3);
+            var opCode = line.Substring(30, 1);
+            var loanCode = line.Substring(31, 1);
+            var amount = line.Substring(35, 6);
+            var fileDate = line.Substring(48, 4);
+            var fileNumber = line.Substring(54, 9);
+            sql.InsertBody(accountNumber, nameCode, opCode, loanCode,
+                amount, fileDate, fileNumber);
+            return;
+        }
+
+        private static void ProcessTrailer(SQLDataAccess sql, string line)
+        {
+            //003E    2001103//2  00001{        \
+            var fileDate = line.Substring(8, 5);
+            var fileNRecords = line.Substring(20, 6);
+
+            sql.InsertTrailer(fileDate, fileNRecords);
+            return;
+        }
+
+        private static void ProcessHeader(SQLDataAccess sql, string line)
+        {
+            //008E    2001103**2 \
+            var staticData1 = line.Substring(0, 4);
+            var fileDate = line.Substring(8, 5);
+            var staticData2 = line.Substring(13, 2);
+
+            sql.InsertHeader(fileDate);
+
+            return;
+        }
+
+        private static char GetSignedNumeric(int input)
+        {
+            var cRet = '{';
+            switch (input)
+            {
+                case 1:
+                    cRet = 'A';
+                    break;
+                case 2:
+                    cRet = 'B';
+                    break;
+                case 3:
+                    cRet = 'C';
+                    break;
+                case 4:
+                    cRet = 'D';
+                    break;
+                case 5:
+                    cRet = 'E';
+                    break;
+                case 6:
+                    cRet = 'F';
+                    break;
+                case 7:
+                    cRet = 'G';
+                    break;
+                case 8:
+                    cRet = 'H';
+                    break;
+                case 9:
+                    cRet = 'I';
+                    break;
+                default:
+                    cRet = '{';
+                    break;
+            }
+            return cRet;
+        }
+    }
+}
+
+/*
+ *
+ * public static class Program
+{
+    private static IConfigurationRoot Configuration { get; set; }
+
+    public static void Main()
+    {
+        IConfigurationBuilder builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables();
+        Configuration = builder.Build();
+
+        IServiceCollection services = new ServiceCollection();
+        services.AddDbContext<MyDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        services.AddScoped<IMyService, MyService>();
+        IServiceProvider provider = services.BuildServiceProvider();
+
+        IMyService myService = provider.GetService<IMyService>();
+        myService.SomeMethod();
+    }
+
+    public class TemporaryDbContextFactory : IDesignTimeDbContextFactory<MyDbContext>
+    {
+        public MyDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationBuilder configBuilder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+            IConfigurationRoot configuration = configBuilder.Build();
+            DbContextOptionsBuilder<MyDbContext> builder = new DbContextOptionsBuilder<MyDbContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new MyDbContext(builder.Options);
+        }
+    }
+ *
+ *
+ */
